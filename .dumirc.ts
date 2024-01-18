5acfa3ae39bbeb2b6f43e5e203e776b54c98df44
minglu <richie_h@163.com>
2024-01-18 15:51:53 +0800

    ci: github-pages

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: 'starry-sea',
+  publicPath: 'starry-sea',
   themeConfig: {
     name: 'starry-sea',
   },
---
+++ docs-dist/nm__dumi__dist__client__pages__Demo__index.28ce73b9.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkstarry_sea=self.webpackChunkstarry_sea||[]).push([[9],{65149:function(v,a,e){e.r(a),e.d(a,{default:function(){return o}});var n=e(27544),t=e(67294),s=function(){var u=(0,n.UO)(),d=u.id,m=(0,n.WF)(),c=m.demos,f=c[d]||{},r=f.component;return r&&(0,t.createElement)(r)},o=s}}]);
